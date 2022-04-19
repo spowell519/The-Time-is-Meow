ca562dd4071dc592511d52b60109d53fa7ea7da0
Mab Bene <outpt@mac.com>
2022-04-19 14:00:13 -0400

    Update ItemPreview.js
    Framework for ItemPreview

--- app/Components/ItemPreview.js
+++ app/Components/ItemPreview.js
@@ -1,3 +1,17 @@
 // individual element in ItemGrid
 
 import React from 'react';
+import { connect } from 'react-redux';
+
+class ItemPreview extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div></div>
+      )
+  }
+}
+
+export default ItemPreview;
