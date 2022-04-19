a4c16241e065b291efd8147d50a2c31db974ae69
Mab Bene <outpt@mac.com>
2022-04-19 14:24:39 -0400

    Update ItemGrid.js
    Framework for ItemGrid

--- app/Components/ItemGrid.js
+++ app/Components/ItemGrid.js
@@ -1,1 +1,26 @@
+// section in page with a grid of items
+
 import React from 'react';
+import { connect } from 'react-redux';
+
+import ItemPreview from './ItemPreview';
+
+class ItemGrid extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const items = this.props.itemList || [];
+
+    return (
+      <section>
+        <h2>Our Products</h2> // change val if using off main page
+          <div id="all-products">
+          items.map(item => <ItemPreview key="{`item_${item.id}`}" item={item} />
+          </div>
+      </section>
+    )
+  }
+}
+
+export default ItemGrid;
